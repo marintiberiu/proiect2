7aaa12e577f3bf1cfc025637f8400a4f119d0278
Tibi <tibi9876@yahoo.com>
2018-01-07 21:05:57 +0200

    v6

--- comanda.js
+++ comanda.js
@@ -36,8 +36,30 @@ function updateTotal() {
     document.getElementById("total").innerHTML = total;
 }
 
+function addMinDate() {
+    if (requestData.readyState === XMLHttpRequest.DONE && document.getElementById("data").value == "")
+    {
+        var dataMin = requestData.response.args.data;
+        var dataInput = document.createElement("input");
+        dataInput.type = "date";
+        dataInput.id = "data";
+        dataInput.min = dataMin;
+        dataInput.value = dataMin;
+        document.getElementById("input-data").removeChild(document.getElementById("data"));
+        document.getElementById("input-data").appendChild(dataInput);
+    }
+}
+
+function getMinDate() {
+    requestData = new XMLHttpRequest();
+    requestData.open("GET", "https://httpbin.org/get?data=2017-01-10", true);
+    requestData.responseType = "json";
+    requestData.onreadystatechange = addMinDate;
+    requestData.send();
+}
+
+getMinDate();
 updateTotal();
-today = new Date();
 document.getElementById("submit").onclick=submit;
 document.addEventListener("click",updateTotal);
 document.addEventListener("keypress",updateTotal);
